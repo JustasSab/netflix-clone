7dfc465ebacd73e00cbe4b29e4adece7c9402463
JustasSab <sabaliunasjustas@gmail.com>
2022-08-09 17:37:05 +0300

    Added (un)Mute button

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,19 @@ import TrailerLogo from './assets/Narcos-Logo.png';
 import TrailerVideo from './assets/Narcos.mp4';
 import './header.css';
 import { FaPlay, FaInfoCircle } from 'react-icons/fa';
+import { useState} from 'react';
+import VolumeOffIcon from '@mui/icons-material/VolumeOff';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 
 const Header = () => {
+    const [audioSwitcher, setAudioSwitcher] = useState(true);
+    const handleChange = () => {
+        return setAudioSwitcher(!audioSwitcher);
+      };
+
     return (
         <div className="header">
-            <video id="videoTrailer" autoPlay muted loop>
+            <video id="videoTrailer" muted={audioSwitcher} autoPlay loop>
                 <source src={TrailerVideo} type="video/mp4"/>
             </video>
             <div class="trailer-content">
@@ -16,6 +24,12 @@ const Header = () => {
                 </button>
                 <button className="btn" id="InfoButton" onClick="myFunction()">
                     <FaInfoCircle/> <span>More Info</span> 
+                </button>
+                <button 
+                    id="muteButton"
+                    onClick={() => handleChange(true)} 
+                    className={( audioSwitcher ? "active" : "")}>
+                    { audioSwitcher ? <VolumeOffIcon/> : <VolumeUpIcon/>}
                 </button>
             </div>
         </div>
